e326468f99a4ac3009fcfb012b1610673a106681
Ariel Zhen <zhenpingping@live.com>
2018-09-24 18:32:58 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,10 @@ class StudentsController < ApplicationController
   end
 
   def activate
-
+    set_student
+    @student.active = !@student.active
+    @student.save 
+    redirect_to student_path(@student) 
   end 
 
   private
